326e869b56a0ca0a42b130c22a066bdf54d7c867
craig.gers <craig.gers@EMEAGBRABXLT252>
2018-11-21 08:46:31 +0000

    Just shell. No code implemented.

---
+++ Encrypter/Encrypter/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Encrypter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemFileSeperator = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItemFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "frmMainMS";
+            // 
+            // fileToolStripMenuItemFile
+            // 
+            this.fileToolStripMenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemFileSeperator,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItemFile.Name = "fileToolStripMenuItemFile";
+            this.fileToolStripMenuItemFile.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItemFile.Text = "&File";
+            // 
+            // toolStripMenuItemFileSeperator
+            // 
+            this.toolStripMenuItemFileSeperator.Name = "toolStripMenuItemFileSeperator";
+            this.toolStripMenuItemFileSeperator.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(3, 42);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(134, 13);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Type text in the box below:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(3, 210);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(78, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Encrypted text:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(239, 200);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(119, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt Text Above";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(6, 58);
+            this.tbxInput.Multiline = true;
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxInput.Size = new System.Drawing.Size(572, 110);
+            this.tbxInput.TabIndex = 4;
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Location = new System.Drawing.Point(6, 239);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxOutput.Size = new System.Drawing.Size(572, 110);
+            this.tbxOutput.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Silly Encryptor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItemFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItemFileSeperator;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.TextBox tbxOutput;
+    }
+}
+
---
+++ Encrypter/Encrypter/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Encrypter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
